fabcfb30d9a4f28c5f9a7791624a534ef0fce8c7
Surajeet Sen <surajeetsen@yahoo.co.in>
2017-08-24 01:32:02 +0530

    Initial Commit

---
+++ JavaInterview/src/string/StringHasAllUniqueChars.java
@@ -0,0 +1,47 @@
+package string;
+
+/**
+ * Implement an algorithm to determine if a string has all unique characters.
+ * (Ref: Cracking the coding interview) 
+ * 
+ * @author Surajeet Sen
+ */
+public class StringHasAllUniqueChars {
+	public static void main(String[] args) {
+		StringHasAllUniqueChars unique = new StringHasAllUniqueChars();
+		System.out.println(unique.areAllUniqueChars1("abcdef12345*&^%"));
+		System.out.println(unique.areAllUniqueChars2("abcdefa"));
+	}
+
+	// If the string is composed of ASCII characters
+	public boolean areAllUniqueChars1(String str) {
+		if (str.length() > 256) {
+			return false;
+		}
+
+		boolean[] charSet = new boolean[256];
+		for (int i = 0; i < str.length(); i++) {
+			if (charSet[str.charAt(i)]) {
+				return false;
+			}
+			charSet[str.charAt(i)] = true;
+		}
+		return true;
+	}
+
+	// If the string only contains characters a-z (only lower case)
+	public boolean areAllUniqueChars2(String str) {
+		int checker = 0;
+
+		for (int i = 0; i < str.length(); i++) {
+			int val = str.charAt(i) - 'a';
+
+			if ((checker & (1 << val)) > 0) {
+				return false;
+			}
+
+			checker |= (1 << val);
+		}
+		return true;
+	}
+}
