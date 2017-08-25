f04e6454046089b399f79fc4f0ac3efa243ada3e
Surajeet Sen <surajeetsen@yahoo.co.in>
2017-08-26 01:10:40 +0530

    Initial commit.

---
+++ JavaInterview/src/string/CheckAnagram.java
@@ -0,0 +1,31 @@
+package string;
+
+/**
+ * Determine if a string is an anagram of another string.
+ * 
+ * @author Surajeet Sen
+ */
+public class CheckAnagram {
+	public static void main(String[] args) {
+		System.out.println(new CheckAnagram().isAnagram("listen", "silent"));
+	}
+	
+	public boolean isAnagram(String s1, String s2) {
+		if(s1.length() != s2.length()) {
+			return false;
+		}
+		
+		int[] noOfChars = new int[256];
+		
+		for(int i = 0; i < s1.length(); i++) {
+			noOfChars[(int) s1.charAt(i)]++;
+		}
+		
+		for(int i = 0; i < s2.length(); i++) {
+			if(--noOfChars[(int) s2.charAt(i)] < 0) {
+				return false;
+			}
+		}
+		return true;
+	}
+}
