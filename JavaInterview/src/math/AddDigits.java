cab0fc98017d0c49a7111a6c2aa0d2fe396bc0dc
Surajeet Sen <surajeetsen@yahoo.co.in>
2017-09-02 00:08:14 +0530

    Refactored code.

--- JavaInterview/src/math/AddDigits.java
+++ JavaInterview/src/math/AddDigits.java
@@ -12,7 +12,6 @@ package math;
  *
  */
 public class AddDigits {
-	
 	public int addDigits(int num) {
         if(num == 0) {
             return 0;
@@ -22,10 +21,4 @@ public class AddDigits {
         
         return (s>0)? s : 9;
     }
-	
-	public static void main(String[] args) {
-		AddDigits ad = new AddDigits();
-		System.out.println(ad.addDigits(38));
-	}
-	
 }
