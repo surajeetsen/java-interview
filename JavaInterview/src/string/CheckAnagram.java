63cabe29164e18d37c54780246df4a40bb7b5671
Surajeet Sen <surajeetsen@yahoo.co.in>
2017-09-04 01:06:26 +0530

    Added junit test.

--- JavaInterview/src/string/CheckAnagram.java
+++ JavaInterview/src/string/CheckAnagram.java
@@ -6,10 +6,6 @@ package string;
  * @author Surajeet Sen
  */
 public class CheckAnagram {
-	public static void main(String[] args) {
-		System.out.println(new CheckAnagram().isAnagram("listen", "silent"));
-	}
-	
 	public boolean isAnagram(String s1, String s2) {
 		if(s1.length() != s2.length()) {
 			return false;
