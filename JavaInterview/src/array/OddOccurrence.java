9fb7fa2926638243e1774a0f315e1575e909dde4
Surajeet Sen <surajeetsen@yahoo.co.in>
2017-09-07 00:53:08 +0530

    Inital Commit.

---
+++ JavaInterview/src/array/OddOccurrence.java
@@ -0,0 +1,32 @@
+package array;
+
+/**
+ * In an array having positive integers, except for one number which occurs odd
+ * number of times, all other numbers occur even number of times. Find the
+ * number.
+ * 
+ * @author Surajeet Sen
+ */
+public class OddOccurrence {
+	
+	public static void main(String[] args) {
+		int[] nums = {1,1,1,2,2,2};
+		System.out.println(new OddOccurrence().getNumberOccuringOddNumberOfTimes(nums));
+	}
+	
+	public int getNumberOccuringOddNumberOfTimes(int[] nums) {
+		if(nums == null || nums.length == 0 || nums.length % 2 == 0) {
+			return -1;
+		} else if(nums.length == 1) {
+			return nums[0];
+		}
+		
+		int num = 0;
+		
+		for(int i : nums) {
+			num = num ^ i;
+		}
+		
+		return num;
+	}
+}
